51f59afe8e26ed39967d0a4c4cccfe32691e2ce2
Aanya Panigrahi <aanya2614@gmail.com>
2024-11-22 08:32:33 +0530

    React project

---
+++ src/assets/assets.js
@@ -0,0 +1,95 @@
+import logo from './logo.svg'
+import logo_dark from './logo_dark.svg'
+import cross_icon from './cross_icon.svg'
+import menu_icon from './menu_icon.svg'
+import star_icon from './star_icon.svg'
+import left_arrow from './left_arrow.svg'
+import right_arrow from './right_arrow.svg'
+import header_img from './header_img.png'
+import brand_img from './brand_img.png'
+import project_img_1 from './project_img_1.jpg'
+import project_img_2 from './project_img_2.jpg'
+import project_img_3 from './project_img_3.jpg'
+import project_img_4 from './project_img_4.jpg'
+import project_img_5 from './project_img_5.jpg'
+import project_img_6 from './project_img_6.jpg'
+import profile_img_1 from './profile_img_1.png'
+import profile_img_2 from './profile_img_2.png'
+import profile_img_3 from './profile_img_3.png'
+
+export const assets = {
+    logo,
+    logo_dark,
+    cross_icon,
+    menu_icon,
+    star_icon,
+    header_img,
+    brand_img,
+    project_img_1,
+    project_img_2,
+    project_img_3,
+    project_img_4,
+    left_arrow,
+    right_arrow,
+}
+
+export const projectsData = [
+    {
+      title: "Email Marketing",
+      descr: "promotional media campaigns",
+      image: project_img_1
+    },
+    {
+      title: "Analytics",
+      descr: "Analytics and Data Analysis",
+      image: project_img_2
+    },
+    {
+      title: "PPC Ads",
+      descr: "creates pay per click advertisements",
+      image: project_img_3
+    },
+    {
+      title: "Content Creation",
+      descr: "Captions Writing and attractive Headlines",
+      image: project_img_4
+    },
+    {
+      title: "SEO",
+      descr: "Enhances visibilty on search - SERPs",
+      image: project_img_5
+    },
+    {
+      title: "Planning",
+      descr: "Marketing Strategies tailored to needs",
+      image: project_img_6
+    },
+    
+  ];
+
+  export const testimonialsData = [
+    {
+        name: "Adam Watson, 34",
+        title: "STartUp Owner",
+        image: profile_img_1,
+        alt: "Portrait of Adam",
+        rating: 5,
+        text: "High standard and excellent quality of work from MDM. They helped my business grow in digital market."
+    },
+    {
+        name: "Richard Nelson, 35",
+        title: "Home Designer",
+        image: profile_img_2,
+        alt: "Portrait of Richard Nelson",
+        rating: 5,
+        text: "From the very first meeting, they understood my vision and helped me reach my aim of growing in media."
+    },
+    {
+        name: "Loshe, 29",
+        title: "Co-Founder Bigway Enterprises",
+        image: profile_img_3,
+        alt: "Portrait of .Random",
+        rating: 4,
+        text: "Visible growth in Business through Media & SEO strategies. The company is more publically known now."
+    }
+];
---
+++ src/components/About.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const About = () => {
+  return (
+    <div className='flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-30 w-full overflow-hidden' id='About'>
+        <h1 className='text-2xl sm:text-4xl font-bold mb-2'>About <span className='underline underline-offset-4 decoration-1 under font-light'>Our Brand</span></h1>
+        <p className='text-gray-500 max-w-80 text-center mb-8'>Your business, our strategy, unmatched results</p>
+        <div className='flex flex-col md:flex-row items-center md:items-start md:gap-20 '> 
+            <img src={assets.brand_img} alt="" className='w-full sm:w-1/2 max-w-lg'/>
+            <div className='flex flex-col items-center md:items-start mt-10 text-gray-600'>
+                <div className='grid grid-cols-2 gap-6 md:gap-10 w-full 2xl:pr-28'>
+                    <div>
+                        <p className='text-4xl font-medium text-gray-800'>15+</p>
+                        <p>Years of Experience</p>
+                    </div>
+                    <div>
+                        <p className='text-4xl font-medium text-gray-800'>4.8K+</p>
+                        <p>Jobs completed</p>
+                    </div>
+                    <div>
+                        <p className='text-4xl font-medium text-gray-800'>3.6K+</p>
+                        <p>Global Clients</p>
+                    </div>
+                    <div>
+                        <p className='text-4xl font-medium text-gray-800'>100+</p>
+                        <p>Awards in the field</p>
+                    </div>
+
+                </div>
+                <p className='my-10 max-w-lg'>We are a results-driven digital marketing agency, offering expert services in SEO, content marketing, social media, PPC, and more. Our tailored strategies help businesses increase visibility, drive traffic, and boost conversions. We focus on delivering measurable results that elevate your brand and achieve your marketing goals effectively.</p>
+                <button className='bg-blue-600 text-white px-8 py-2 rounded'>Learn More</button>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
